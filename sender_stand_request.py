e4afe67169b9f68fa0022f54659c43e1f85eb2a7
Konstantin <konsugar@Air-Fixed.lan>
2024-07-01 13:40:36 +0200

    My first commit

---
+++ sender_stand_request.py
@@ -0,0 +1,33 @@
+import configuration
+import requests
+import data
+
+
+# Функция для получения данных из таблицы пользователей
+def get_users_table():
+    # Составление полного URL пути к данным таблицы пользователей
+    # путем конкатенации базового URL сервиса и конечной точки таблицы пользователей
+    # Возвращает объект ответа от сервера
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+
+# Определение функции post_new_user для отправки POST-запроса на создание нового пользователя
+def post_new_user(body):
+    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
+    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
+    # json=body используется для отправки данных пользователя в формате JSON
+    # headers=data.headers устанавливает заголовки запроса из модуля data
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,  # Тело запроса содержит ID продуктов в формате JSON
+                         headers=data.headers)
+
+
+
+
+
